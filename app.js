5a932742e1826ef029f17bf2236392788568af93
Dajve Echols <dajvelechols@gmail.com>
2022-03-17 14:47:30 -0500

    generates random array everytime user presses reset

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const cards = [{
+var arrayOfCards = [{
     name: 'Adidas',
     img: 'images.png'
 }, {
@@ -36,10 +36,39 @@ const cards = [{
     img: 'offWhite.png'
 }]
 
-cards.sort(() => 0.5 - Math.random())
+shuffle(arrayOfCards)
+console.log(arrayOfCards);
+
+
 var reset = false
 var counter = 0
 var hasStarted = true
+
+//game stats
+var score = 0
+var matched = 0
+var tries = 0
+var isP = false
+var chosenCards = []
+
+
+//randomize array
+function shuffle(array) {
+    let currentIndex = array.length,
+        randomIndex;
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]
+        ];
+    }
+    return array;
+}
 
 var createBoard = () => {
     var parentDiv = document.querySelector('#gameContainer')
@@ -51,6 +80,10 @@ var createBoard = () => {
         } else if (e.target.id == 'reset') {
             reset = true
             resetBoard()
+        } else if (e.target.className == 'cards') {
+            //flip the card of which ever image is under that specific card
+            //then add that card to an empty array called chosenCards
+            //then add a increment to tries variable
         }
     })
 }
@@ -58,7 +91,7 @@ var createBoard = () => {
 function start() {
     if (counter < 1 && hasStarted == true) {
         counter++
-        for (let i = 0; i < cards.length; i++) {
+        for (let i = 0; i < arrayOfCards.length; i++) {
             let currentCard = document.createElement('div')
             currentCard.setAttribute('id', 'card' + i)
             currentCard.setAttribute('class', 'cards')
@@ -71,20 +104,21 @@ function start() {
     hasStarted = false
 }
 
+//to clear the welcome text
 function clearBoard() {
     var welcomeText = document.querySelector('.WelcomeHeader')
     welcomeText.style.display = 'none'
     var pressToStart = document.querySelector('#pressStart')
     pressToStart.style.display = 'none'
 }
-
+//add either event listeners or put an event listener on the parent div
 function resetBoard() {
     reset = true
     if (reset == true && counter <= 1) {
         counter = 0
         reset = false
-        let cards = document.querySelectorAll('.cards')
-        cards.forEach(card => {
+        let cardDivs = document.querySelectorAll('.cards')
+        cardDivs.forEach(card => {
             if (card.style.animationName != "resetTransition" && card.style.animationName != "resetTransition2") {
                 card.style.animationName = "resetTransition"
                 card.style.animationDuration = "1.5s"
@@ -97,8 +131,14 @@ function resetBoard() {
                 card.style.animationName = "resetTransition"
             }
         })
+
+        //YOU WHERE WORKING ON THIS RIGHT HERE, THE COMPUTER DOESNT RECOGNIZE THE sort() , INSIDE OUT generateRandom()
+        //have to find out why
+        shuffle(arrayOfCards)
+            // console.log(cardDivs);
+        console.log(arrayOfCards);
     } else {
-        console.log("this is in the else statement in resetBoard() function");
+        console.error("this is in the else statement in resetBoard() function");
     }
 }
 
